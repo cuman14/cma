67bf2c1b3e120765136a09bc0798cddafe259760
Oswald <cuman74@gmail.com>
2022-01-05 23:50:19 +0100

    dababase implement

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,15 +1,17 @@
 import { MENU } from './../../../../data/menu/menu.constants';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { Menu } from 'src/app/core/modules/menu/models/menu.model';
+import { ToolUtil } from 'src/app/core/utils/ToolUtils';
 
 @Component({
   selector: 'cma-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit  {
 
   public menu: Menu[] = MENU;
+  isMobile = ToolUtil.isMobile();
   constructor() { 
   }
 

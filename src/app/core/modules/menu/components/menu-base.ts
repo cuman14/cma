a703a3e01ec630af465265cf1ed04017c262dec6
Oswald <cuman74@gmail.com>
2022-01-06 18:05:03 +0100

    home maqueta

--- src/app/core/modules/menu/components/menu-base.ts
+++ src/app/core/modules/menu/components/menu-base.ts
@@ -2,36 +2,33 @@ import { Menu } from './../models/menu.model';
 import { Input, Component } from '@angular/core';
 import { MenuService } from '../services/menu.service';
 @Component({
-    selector: 'cma-menu-base',
-    template: `<div></div>`
+  selector: 'cma-menu-base',
+  template: `<div></div>`,
 })
 export class MenuBase {
-    
-   menu!: Menu[];
-    toggle: boolean = true;
-    _indexCurrentMenu: number |null = null;
-     
-    constructor(private _menuService: MenuService) { }
+  menu!: Menu[];
+  toggle: boolean = true;
+  _indexCurrentMenu: number | null = null;
 
+  constructor(private _menuService: MenuService) {}
 
-    getMenu() {
-        this._menuService.getMenu().subscribe(data=> {
-            this.menu = data;
-        });  
-    }
+  getMenu() {
+    this._menuService.getMenu().subscribe((data) => {
+      this.menu = data;
+    });
+  }
 
-    openMenu() {
-        this.toggle = !this.toggle;
-        this._indexCurrentMenu = null;
-      }
-    
-      _selectItem(index: number, event: Event): void {
-        this._indexCurrentMenu = index;
-        event.stopPropagation();
-      }
-      _closeItem( event: Event) {
-        this._indexCurrentMenu = null;
-        event.stopPropagation();
-      }
+  openMenu() {
+    setTimeout(() => (this.toggle = !this.toggle));
+    this._indexCurrentMenu = null;
+  }
 
+  _selectItem(index: number, event: Event): void {
+    this._indexCurrentMenu = index;
+    event.stopPropagation();
+  }
+  _closeItem(event: Event) {
+    this._indexCurrentMenu = null;
+    event.stopPropagation();
+  }
 }

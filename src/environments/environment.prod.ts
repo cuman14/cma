680dbce1f4515a735a4bf6b5ef5ff522b8bb2b54
Oswald <cuman74@gmail.com>
2022-01-17 22:08:45 +0100

    fix error enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,9 @@ export const environment = {
     appId: "1:584876016044:web:142872d90c407dc29ffa09",
     measurementId: "G-85524VGG88"
   },
+  contentFul: {
+    spaceId: "ttxv6n5w47ni",
+    token: 'igzcz7bFSqdeC55Q2KRb079IiGitcHtkAElS1c7XdhU'
+  },
   production: true
 };

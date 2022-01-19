1126b5f67d14bf9187f961ce802e5a036e88a343
Oswald <cuman74@gmail.com>
2022-01-19 20:03:29 +0100

    update packege and cms

--- src/app/modules/content/content.service.ts
+++ src/app/modules/content/content.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as contentful from 'contentful';
+import { from } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -16,6 +17,6 @@ export class ContentService {
   logContent(contentId: string) {
   /*   this._client.getEntries({content_type: 'cma'}).then(res => console.log(res)); */
 
-    this._client.getEntry('3rqaxN0WyVQYHPk1L22Ay0').then(res => console.log(res))
+    return from(this._client.getEntry('3rqaxN0WyVQYHPk1L22Ay0'))
   }
 }
--- src/app/modules/content/content/content.component.ts
+++ src/app/modules/content/content/content.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ContentService } from '../content.service';
-
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'cma-content',
   templateUrl: './content.component.html',
@@ -8,10 +8,14 @@ import { ContentService } from '../content.service';
 })
 export class ContentComponent implements OnInit {
 
+  data: any;
   constructor(private readonly _contentService: ContentService) { }
 
   ngOnInit(): void {
-    this._contentService.logContent('3rqaxN0WyVQYHPk1L22Ay0');
+    this._contentService.logContent('3rqaxN0WyVQYHPk1L22Ay0').pipe(map((res) => res.fields)).subscribe(data => {
+      this.data = data;
+      console.log(this.data);
+    });
   }
 
 }
